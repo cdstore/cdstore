91a4a696b1b35412f10ebdc8bba743b85dfc7078
Daniel Antwi <reckdan@gmail.com>
2011-10-17 11:58:57 -0400

    updates

---
+++ CDStore/src/filter/SessionFilter.java
@@ -0,0 +1,66 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class SessionFilter
+ */
+public class SessionFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public SessionFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		 
+		//Block of Code By : tgiunipero
+		HttpServletRequest req = (HttpServletRequest) request;
+
+	        HttpSession session = req.getSession(false);
+
+	        // if session doesn't exist, forward user to welcome page
+	        if (session == null) {
+	            try {
+	                req.getRequestDispatcher("/index.jsp").forward(request, response);
+	            } catch (Exception ex) {
+	                ex.printStackTrace();
+	            }
+	            return;
+	        }
+
+	      
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
--- cdstoreproject/CatalogService/src/com/cdstore/catalogservice/ProductCatalog.java
+++ cdstoreproject/CatalogService/src/com/cdstore/catalogservice/ProductCatalog.java
@@ -1,5 +1,7 @@
 package com.cdstore.catalogservice;
 
 public class ProductCatalog {
-
+	public String getproduct(String name){
+		return "You have submitted " + name;
+	}
 }
